db1ef49e2585c4c588cc3c655d848e15a3242ff1
maremo <maremo@taltech.ee>
2026-02-20 22:54:10 +0200

    Initial project commit

---
+++ src/main/java/com/example/CGI_Restaurant/CgiRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.example.CGI_Restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgiRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgiRestaurantApplication.class, args);
+	}
+
+}
